21d0b73b789950120a68f6d78336f10c1c6106d5
GeeksforGeeksleonidkoltan <leonidkoltan@gmail.com>
2023-03-08 12:04:25 +0500

    8

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,31 +1,27 @@
-import Head from 'next/head'
 import { serialize } from 'next-mdx-remote/serialize';
 import { MDXRemote } from 'next-mdx-remote';
 import { getAllPostsPath, getPostData } from '@/utils/getPostsData.js';
 import Button from '@/components/button';
 //import Link from 'next/link'
-import getOgImage from '@/utils/getOgImage';
-
 
 const components = {
   Button
 }
 
-export default function Blog({ postContent, ogImage, postMetadata }) {
+export default function Blog({ postContent }) {
 
     return (
         <>
         <Head>
             <title>Next.js | Tech Radar</title>
-            <meta name="og:image" content={ogImage} />
         </Head>
         <div>
             <div className = 'blog-content'>
-                {postMetadata.title}
                 <MDXRemote {...postContent} components = {components} />
             </div>
         </div>
         </>
+
     )
 }
 
@@ -37,18 +33,14 @@ export async function getStaticPaths() {
     }
 }
 
-export async function getStaticProps({ params, slug, title }) {
+export async function getStaticProps({ params }) {
     const postData = await getPostData(params.slug);
     const mdxSource = await serialize(postData.content);
-    const ogImage = await getOgImage(
-        `/phiilu.com?title=${title}&url=${process.env.BASE_URL}/${slug}`
-      );
     return {
         props: {
             postMetadata: postData.metadata,
             postContent: mdxSource,
             slug: params.slug,
-            ogImage
         }
     }
 
