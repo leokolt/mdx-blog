fe43612bc46f0fe9570f6e285023a6a9f3027e46
GeeksforGeeksleonidkoltan <leonidkoltan@gmail.com>
2023-03-08 12:05:53 +0500

    9

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import { serialize } from 'next-mdx-remote/serialize';
 import { MDXRemote } from 'next-mdx-remote';
 import { getAllPostsPath, getPostData } from '@/utils/getPostsData.js';
