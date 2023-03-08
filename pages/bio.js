6f681e0b93215dc27b572df85d41eda79265c2fc
GeeksforGeeksleonidkoltan <leonidkoltan@gmail.com>
2023-03-08 11:32:24 +0500

    super

--- pages/bio.js
+++ pages/bio.js
@@ -2,7 +2,7 @@ const Bio = () => {
     return (
       <div className="mt-3">
         <p className="display-4 text-center">Im Ebenezer Don</p>
-        <p className="text-center">Ebenezer Don's bio here</p>
+        <p className="text-center">Ebenezer Dons bio here</p>
       </div>
     )
   }
